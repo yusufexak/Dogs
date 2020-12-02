ebe16726b30b95ff6603664e0ad9be909c1d9a70
yusufexak <yusufsirinn@gmail.com>
2020-12-02 21:37:50 +0300

    init

---
+++ app/src/main/java/com/srn/dogs/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.srn.dogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
