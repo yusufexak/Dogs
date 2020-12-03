dbaf67cec3df8b26f47799574c5186f133bde8b6
yusufexak <yusufsirinn@gmail.com>
2020-12-03 20:45:44 +0300

    dog list fragment connected

--- app/src/main/java/com/srn/dogs/view/DogListFragment.kt
+++ app/src/main/java/com/srn/dogs/view/DogListFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.srn.dogs.R
+import com.srn.dogs.adapter.DogRecyclerAdapter
+import com.srn.dogs.viewModel.DogListViewModel
 import kotlinx.android.synthetic.main.fragment_dog_list.*
 
 
 class DogListFragment : Fragment() {
+    private lateinit var viewModel:DogListViewModel
+    private val recyclerDogAdapter=DogRecyclerAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,8 @@ class DogListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(DogListViewModel::class.java)
+        viewModel.refleshData()
     }
 
 
