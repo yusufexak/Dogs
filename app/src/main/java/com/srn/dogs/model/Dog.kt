1f440115161639176c3f6485110d7e0472a6de6b
yusufexak <yusufsirinn@gmail.com>
2020-12-04 20:04:53 +0300

    api interface create

--- app/src/main/java/com/srn/dogs/model/Dog.kt
+++ app/src/main/java/com/srn/dogs/model/Dog.kt
@@ -1,7 +1,12 @@
 package com.srn.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Dog(
+    @SerializedName("code")
     val code:String?,
+    @SerializedName("description")
     val description:String?,
+    @SerializedName("imageUrl")
     val imageUrl:String?) {
 }
---
+++ app/src/main/java/com/srn/dogs/service/IDogAPI.kt
@@ -0,0 +1,13 @@
+package com.srn.dogs.service
+
+import com.srn.dogs.model.Dog
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface IDogAPI {
+    //https://hwasampleapi.firebaseio.com/dogs.json
+    //BASE_URL -> hwasampleapi.firebaseio.com/
+
+    @GET("dogs.json")
+    fun getDogs(): Single<List<Dog>>
+}
