7ee0409d4ac26a406d7f4a490e09aa7bc5377374
yusufexak <yusufsirinn@gmail.com>
2020-12-05 19:06:55 +0300

    model change

--- app/src/main/java/com/srn/dogs/model/Dog.kt
+++ app/src/main/java/com/srn/dogs/model/Dog.kt
@@ -1,12 +1,23 @@
 package com.srn.dogs.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Dog(
+    @ColumnInfo(name="code")
     @SerializedName("code")
     val code:Int?,
+
+    @ColumnInfo(name="description")
     @SerializedName("description")
     val description:String?,
+
+    @ColumnInfo(name="imageUrl")
     @SerializedName("imageUrl")
     val imageUrl:String?) {
+    @PrimaryKey(autoGenerate = true)
+    var uuid:Int =0
 }
